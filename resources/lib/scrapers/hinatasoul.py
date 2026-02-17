fde4925c88015a139c8d6347db12c7294c0139bf
Ícaro Maicon <46580318+icarok99@users.noreply.github.com>
2026-02-17 11:49:39 -0300

    Update hinatasoul.py

--- resources/lib/scrapers/hinatasoul.py
+++ resources/lib/scrapers/hinatasoul.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 WEBSITE = 'HINATASOUL'
 
 import re
@@ -19,14 +20,12 @@ session.headers.update({
 
 from resources.lib.resolver import Resolver
 
-# Importar strings de tradução do Kodi
 try:
     import xbmcaddon
     addon = xbmcaddon.Addon()
-    DUBBED = addon.getLocalizedString(30200)  # "DUBBED"
-    SUBTITLED = addon.getLocalizedString(30202)  # "SUBTITLED"
+    DUBBED = addon.getLocalizedString(30200)
+    SUBTITLED = addon.getLocalizedString(30202)
 except:
-    # Fallback se não estiver no ambiente Kodi
     DUBBED = 'DUBLADO'
     SUBTITLED = 'LEGENDADO'
 
